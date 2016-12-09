423214b51ec9fbf4ec08c90828446b73250ef3e6
刘鎏 <lllliuliu@163.com>
2016-12-09 10:11:46 +0800

    修改sql文件

--- database/sql.sql
+++ database/sql.sql
@@ -1,21 +1,21 @@
 ﻿-- ----------------------------
--- Table structure for onethink_user_notification_broadcast
+-- Table structure for user_notification_broadcast
 -- ----------------------------
-DROP TABLE IF EXISTS `onethink_user_notification_broadcast`;
-CREATE TABLE `onethink_user_notification_broadcast` (
+DROP TABLE IF EXISTS `user_notification_broadcast`;
+CREATE TABLE `user_notification_broadcast` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `title` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT '分类类型的标题',
   `content` text COLLATE utf8_unicode_ci NOT NULL COMMENT '分类类型的内容',
   `created_at` int(10) unsigned NOT NULL COMMENT '创建时间',
   `updated_at` int(10) unsigned NOT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户广播消息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户广播消息表';
 
 -- ----------------------------
--- Table structure for onethink_user_notification_categories
+-- Table structure for user_notification_categories
 -- ----------------------------
-DROP TABLE IF EXISTS `onethink_user_notification_categories`;
-CREATE TABLE `onethink_user_notification_categories` (
+DROP TABLE IF EXISTS `user_notification_categories`;
+CREATE TABLE `user_notification_categories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '标识',
   `title` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT '分类类型的标题',
@@ -25,13 +25,13 @@ CREATE TABLE `onethink_user_notification_categories` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `notification_categories_name_unique` (`name`),
   KEY `notification_categories_name_index` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户系统消息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户系统消息表';
 
 -- ----------------------------
--- Table structure for onethink_user_notifications
+-- Table structure for user_notifications
 -- ----------------------------
-DROP TABLE IF EXISTS `onethink_user_notifications`;
-CREATE TABLE `onethink_user_notifications` (
+DROP TABLE IF EXISTS `user_notifications`;
+CREATE TABLE `user_notifications` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `from_id` bigint(20) unsigned NOT NULL COMMENT '发送者user_id，系统和管理员发送时为0',
   `from_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '发送者类型',
@@ -50,4 +50,4 @@ CREATE TABLE `onethink_user_notifications` (
   KEY `notifications_from_id_index` (`from_id`),
   KEY `notifications_to_id_index` (`to_id`),
   KEY `notifications_category_id_index` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2400 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户消息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户消息表';
